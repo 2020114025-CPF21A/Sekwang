a9a652222a6b32639cbe1aba21578086b0a97138
daeyeon <dynovember05@naver.com>
2025-09-27 16:01:31 +0900

    스프링부트 초기설정

---
+++ src/main/java/Sekwang/SekwangApplication.java
@@ -0,0 +1,13 @@
+package Sekwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekwangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekwangApplication.class, args);
+	}
+
+}
