fc9a9aca7a723f937ad9a28e24a4f184a8724ebf
daeyeon <dynovember05@naver.com>
2025-10-12 03:36:58 +0900

    update health check

---
+++ src/main/java/Sekwang/api/Controller/HealthController.java
@@ -0,0 +1,14 @@
+package Sekwang.api.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public Map<String, String> healthCheck() {
+        return Map.of("status", "UP");
+    }
+}
